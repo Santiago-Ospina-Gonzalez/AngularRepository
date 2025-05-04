5baedaeae21c6a20037a3e3b5064ddda99afedd2
Santiago-Ospina-Gonzalez <147220788+Santiago-Ospina-Gonzalez@users.noreply.github.com>
2025-05-04 22:25:03 +0000

    Agregar encabezado fijo y manejar su visibilidad al hacer scroll; actualizar estilos y estructura del componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TablaComponent } from './tabla/tabla.component';
 import { TablaCursosComponent } from "./tabla-cursos/tabla-cursos.component";
 import { FormularioComponent } from './formulario/formulario.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserComponent, NavbarComponent, TablaComponent,FormularioComponent],
+  imports: [CommonModule,RouterOutlet, UserComponent, NavbarComponent, TablaComponent,FormularioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
   
@@ -16,4 +17,22 @@ import { FormularioComponent } from './formulario/formulario.component';
 export class AppComponent {
   title = 'angular-17-app';
   username = '';
+
+  previousScrollPosition = 0;
+  isHeaderHidden = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScrollPosition = window.pageYOffset;
+
+    if (currentScrollPosition > this.previousScrollPosition && currentScrollPosition > 100) {
+      // Usuario está bajando
+      this.isHeaderHidden = true;
+    } else {
+      // Usuario está subiendo
+      this.isHeaderHidden = false;
+    }
+
+    this.previousScrollPosition = currentScrollPosition;
+  }
 }
